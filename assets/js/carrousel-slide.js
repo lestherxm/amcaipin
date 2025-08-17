7e27bb621f9e2692e9664f14ffda2a2077fe3e54
Lesther <lestherxm@gmail.com>
2025-08-16 23:29:58 -0600

    refactor code

--- assets/js/carrousel-slide.js
+++ assets/js/carrousel-slide.js
@@ -2,11 +2,11 @@ document.addEventListener('DOMContentLoaded', function () {
     var splide = new Splide( '.splide', {
         type: 'loop',           // 🔁 Hace que el slider sea infinito
         autoplay: true,         // ▶️ Activa la reproducción automática
-        interval: 2500,         // ⏱️ 3 segundos entre cada slide (en milisegundos)
+        interval: 3000,         // ⏱️ 3 segundos entre cada slide (en milisegundos)
         pauseOnHover: false,    // 🛑 No se pausa al poner el mouse encima
         pauseOnFocus: false,    // 🛑 No se pausa al enfocar con el teclado
         pagination: true,       // ⚪⚪⚪ Muestra los puntos de paginación
-        // padding: '5rem'
+        arrows: false,          // ⬅️➡️ Oculta las flechas de navegación
     })
     splide.mount();
 });
