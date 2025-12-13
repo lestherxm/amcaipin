e793305452f69aef33bd5974a6d6bc7b921dd076
Lesther <lestherxm@gmail.com>
2025-12-13 17:10:37 -0600

    poker cards implementation

---
+++ assets/js/poker-services.js
@@ -0,0 +1,28 @@
+const pokerCards = document.querySelectorAll(".poker_card");
+let pokerIndex = 0;
+
+function updatePokerStack() {
+    pokerCards.forEach((pokerCard, index) => {
+        pokerCard.classList.remove("poker_card--active");
+
+        const offset =
+        (index - pokerIndex + pokerCards.length) % pokerCards.length;
+
+        pokerCard.style.setProperty("--offset", offset);
+        pokerCard.style.zIndex = pokerCards.length - offset;
+    });
+
+    pokerCards[pokerIndex].classList.add("poker_card--active");
+}
+
+pokerCards.forEach(pokerCard => {
+    const next = () => {
+        pokerIndex = (pokerIndex + 1) % pokerCards.length;
+        updatePokerStack();
+    };
+
+    pokerCard.addEventListener("click", next);
+    pokerCard.addEventListener("touchstart", next, { passive: true });
+});
+
+updatePokerStack();
